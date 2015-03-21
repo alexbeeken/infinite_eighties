bd5691602209dd1c7e67418cd4429a48153a4714
Alex Beeken <aeb242@gmail.com>
2015-03-21 13:05:28 -0700

    Have README and code

---
+++ README.md
@@ -0,0 +1,32 @@
+Infinite Eighties
+===============
+
+This is a song you can copy and paste into Sonic Pi. It reminds me of a groovy eighties song but it's infinitely long and changes as you listen to it.
+
+## Installation
+
+1. Download [Sonic Pi](http://sonic-pi.net/).
+
+2. Copy and paste the code inside and press play.
+
+## Information
+
+This song was made with some simple rhythm randomizing techniques you can check out on [my blog](www.alexbeeken.com). This was intended to add some color to the rhythm randomizations that I was experimenting with in that post.
+
+From a music theory standpoint, this technique of creating and randomizing simple rhythms could be used to create modern pop music. It could never create a melody or have knowledge of how the sections might relate to one another.
+
+## Contributing
+
+1. Fork it!
+
+2. Follow installation instructions to see it running.
+
+3. Make changes.
+
+4. Commit.
+
+5. Submit a pull request.
+
+## License
+
+MIT
---
+++ infinite_eighties.rb
@@ -0,0 +1,156 @@
+scale = [:c4, :d4, :e4, :g4, :a4]
+
+define :intro_drum_beat do
+  use_random_seed 2
+
+  in_thread do
+    1.times do
+      phrase_length = 4
+      sleep_time = 0
+      end_loop = false
+      use_synth :mod_tri
+      until end_loop == true  do
+        sleep_time = 0.125 * (1 + (rand_i(8)))
+        if sleep_time < phrase_length
+          phrase_length = (phrase_length - sleep_time)
+          sleep (sleep_time.abs)
+        else
+          sleep phrase_length
+          end_loop = true
+        end
+
+        sample :drum_snare_hard
+      end
+    end
+  end
+end
+
+
+
+define :drum_bass_kick do
+
+  in_thread do
+    loop do
+      sample :drum_bass_hard
+      sleep 0.5
+    end
+  end
+end
+
+define :snare_and_cymbal do
+
+  in_thread do
+    loop do
+      sample :drum_cymbal_closed
+      sleep 0.25
+    end
+  end
+
+  in_thread do
+    loop do
+      sample :drum_snare_hard
+      sleep 1
+    end
+  end
+
+end
+
+define :melody1 do |times, seed|
+
+  in_thread do
+    times.times do
+      use_random_seed seed
+      phrase_length = 4
+      sleep_time = 0
+      end_loop = false
+      use_synth :sine
+      until end_loop == true  do
+        sleep_time = 0.125 * (1 + (rand_i(8)))
+        if sleep_time < phrase_length
+          phrase_length = (phrase_length - sleep_time)
+          sleep (sleep_time.abs)
+        else
+          sleep phrase_length
+          end_loop = true
+        end
+        play scale.sample(), release: 0.7
+      end
+    end
+  end
+end
+
+define :melody2 do |times, seed|
+
+  in_thread do
+    times.times do
+      use_random_seed seed
+      phrase_length = 4
+      sleep_time = 0
+      end_loop = false
+      use_synth :mod_tri
+      until end_loop == true  do
+        sleep_time = 0.125 * (1 + (rand_i(8)))
+        if sleep_time < phrase_length
+          phrase_length = (phrase_length - sleep_time)
+          sleep (sleep_time.abs)
+        else
+          sleep phrase_length
+          end_loop = true
+        end
+
+        play scale.sample(), release: 0.5
+      end
+    end
+  end
+end
+
+define :breakdown do |length, times, seed|
+  in_thread do
+    times.times do
+      use_random_seed seed
+      phrase_length = length
+      sleep_time = 0
+      end_loop = false
+      use_synth :mod_tri
+      until end_loop == true  do
+        sleep_time = 0.125 * (1 + (rand_i(8)))
+        if sleep_time < phrase_length
+          phrase_length = (phrase_length - sleep_time)
+          sleep (sleep_time.abs)
+        else
+          sleep phrase_length
+          end_loop = true
+        end
+
+        play scale.sample(), release: 0.5
+        sample :drum_bass_hard
+        sample :drum_snare_hard
+        sample :drum_cymbal_hard
+      end
+    end
+  end
+end
+
+
+drum_bass_kick
+intro_drum_beat
+
+sleep 4
+
+snare_and_cymbal
+use_random_seed 2
+
+4.times do
+  melody1(4, rand_i(20000))
+  melody2(4, rand_i(20000))
+  sleep 16
+end
+
+breakdown(4, 4 , 1)
+sleep 16
+
+4.times do
+  melody1(4, rand_i(20000))
+  melody2(4, rand_i(20000))
+  sleep 16
+end
